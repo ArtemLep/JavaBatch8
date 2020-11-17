8f2623a32dcc52ede6ecb7001199e35c95e2339e
ArtemLep <mr.lepinskiy.artem@gmail.com>
2020-11-16 22:04:54 -0500

    Review Class4

--- src/com/syntax/class22/Main.java
+++ src/com/syntax/class22/Main.java
@@ -1,31 +1,24 @@
 package com.syntax.class22;
 
+
+class Parent {
+    void method() {
+        System.out.println("Parent method");
+    }
+}
+
+class Child extends Parent {
+    void method() {
+        System.out.println("Child method");
+    }
+}
+
 public class Main {
-String school;
-    int batch;
-    int years;
-    String LastDayOfSchool;
-
-    Main() {
-    }
-
-    Main(String school,int batch, int years,String LastDayOfSchool) {
-        this.school=school;
-        this.batch=batch;
-        this.years=years;
-        this.LastDayOfSchool=LastDayOfSchool;
-        System.out.println(school+" ");
-    }
-
-
-
-
     public static void main(String[] args) {
-        Main task1 = new Main();
-        task1.school="Syntax";
-        task1.batch=6;
-        task1.years=2020;
-        task1.LastDayOfSchool="07/30/2020";
-        System.out.println(task1.school+" "+task1.batch+" "+task1.years+" "+task1.LastDayOfSchool);
+        Parent parent = new Parent();
+        parent.method();
+        Child child = new Child();
+        child.method();
+    }
 
-}	}
+}
