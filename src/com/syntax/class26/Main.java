d6940372e0da8b483af035565694d977747ce663
ArtemLep <mr.lepinskiy.artem@gmail.com>
2020-11-19 22:21:35 -0500

    ReviewClass5

---
+++ src/com/syntax/class26/Main.java
@@ -0,0 +1,14 @@
+package com.syntax.class26;
+
+public class Main {
+     final void value(){
+         System.out.println("https://syntaxtechs.com");
+
+    }
+
+    public static void main(String[] args) {
+
+         Main main=new Main();
+         main.value();
+    }
+}
